0102a811e3f5ba09e922815f9ecb49e90b5d2e49
Anthony Max <maximosenamaximo@gmail.com>
2024-04-24 20:50:03 -0300

    att: outras melhorias

--- src/js/video.js
+++ src/js/video.js
@@ -4,9 +4,21 @@ var btn = document.getElementById("videoBackBtn");
 function videoBG() {
   if (video.paused) {
     video.play();
-    btn.innerHTML = '<i class="fa-solid fa-pause"></i>';
+    btn.innerHTML = `                        
+    <lord-icon
+    src="https://cdn.lordicon.com/ptvmrrcc.json"
+    trigger="morph"
+    state="morph-play-pause"
+    style="width:25px;height:25px;margin-left:-8px;margin-bottom:-5px;">
+    </lord-icon>`;
   } else {
     video.pause();
-    btn.innerHTML = '<i class="fa-solid fa-play"></i>';
+    btn.innerHTML = `                        
+    <lord-icon
+    src="https://cdn.lordicon.com/ptvmrrcc.json"
+    trigger="morph"
+    state="morph-pause-play"
+    style="width:25px;height:25px;margin-left:-8px;margin-bottom:-5px;">
+    </lord-icon>`;
   }
 }
