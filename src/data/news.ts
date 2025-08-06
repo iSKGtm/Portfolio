1cc283e0dbaa9cae1e7e515dbf1eab9f8b153f00
Anthony Max <maximosenamaximo@gmail.com>
2025-08-06 18:41:38 -0300

    att (1.0.0): add news post

--- src/data/news.ts
+++ src/data/news.ts
@@ -541,7 +541,7 @@ export const newsData: NewsItem[] = [
     imageUrl: "/images/news/posts/dev/novo_website/banner.jpg",
     tags: ["iSKGtm Dev"],
     tagImage: "/images/symb/dev.png",
-    date: "09/01/2025",
+    date: "08/01/2025",
     authorName: "Anthony M.",
     minutesRead: 3,
     content: `
@@ -596,6 +596,119 @@ export const newsData: NewsItem[] = [
         <h2>Como é por dentro do novo website?</h2>
 
         <p>Fizemos ele de uma forma diferente das anteriores, demonstrando profissionalismo e simplicidade, até no código. O site também herda alguns recursos das respectivas versões de 2023 e 2024, como o sistema de navegação e scrolling do site (chama-se internamente de FLUID UI), onde houve apenas melhorias de como ele reage e a mudança visual da barra de navegação, para melhor acesso aos dispositivos móveis. Na parte de CSS, herdamos toda estrutura base do site de 2024, com animações & fontes pré-definidas, apenas melhoramos para melhor visualização.</p>
+
+        <h2>iSKGtm, We Are in Everything</h2>
+      </main>
+    `,
+  },
+
+  {
+    url: "/news/corecycle",
+    title: "CoreCycle",
+    label: "Metodologia CoreCycle.",
+    imageUrl: "/images/news/posts/outros/corecycle/banner.jpg",
+    tags: ["Outros"],
+    tagImage: "/images/symb/outros.png",
+    date: "08/06/2025",
+    authorName: "Anthony M.",
+    minutesRead: 5,
+    content: `
+      <main>
+        <img src="/images/news/tags/iskgtm.jpg" alt="iSKGtm"/>
+        
+        <p>É um fluxo de trabalho, ou metodologia, iniciado em 01 de julho de 2025 a todos os serviços, que visa a reutilização de projetos com elementos modulares, que deram "certo", para otimizar tempo e qualidade de
+        serviço/produção.</p>
+
+        <h2>Anteriormente, como funcionava?</h2>
+
+        <p>Não havia um tipo de fluxograma, sendo assim, podendo ou não recriar projetos do 0,
+        mesmo sem haver a necessidade disto, demandando tempo e reduzindo a qualidade de
+        um projeto em produção, como polimento do mesmo por exemplo.</p>
+
+        <h2>Princípios da CoreCycle</h2>
+        <p>
+          <table>
+            <thead>
+              <tr>
+                <th>Princípio</th>
+                <th>Descrição</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Essência funcional</td>
+                <td>Reaproveitar o serviço ou projeto modularizado.</td>
+              </tr>
+              <tr>
+                <td>Modularidade viva</td>
+                <td>Garantir que tudo possa se transformar e se adaptar.</td>
+              </tr>
+              <tr>
+                <td>Estética iterativa</td>
+                <td>Valorizar a beleza que pode emergir do reaproveitamento e reinvenção.</td>
+              </tr>
+              <tr>
+                <td>Ciclo consciente</td>
+                <td>Evitar retrabalho, priorizando melhorias e polimento.</td>
+              </tr>
+              <tr>
+                <td>Registro contínuo</td>
+                <td>Aprender com os próprios ciclos e alimentar os próximos.</td>
+              </tr>
+            </tbody>
+          </table>
+        </p>
+
+        <h2>Ciclo de Evoução</h2>
+
+        <p>
+          <ul>
+            <li>Monitorar o desempenho dos ativos reaproveitados.</li>
+            <li>Aprimorar os que apresentarem melhor desempenho com base em métricas.</li>
+            <li>Arquivar ou descartar os que não gerarem mais valor.</li>
+          </ul>
+        </p>
+
+        <h2>Como funcionará nos serviços iSKGtm Music?</h2>
+
+        <p>No serviço Music, com REAPER, onde é compatível com este fluxograma, utilizam-se projetos com modulos (pastas) de samples e VST's que podem ser reutilizados com facilidade em produções futuras.</p>
+
+        <img src="/images/news/posts/outros/corecycle/0.jpg" alt="Exemplo de modulos, com REAPER"/>
+
+        <h3>Exemplo de modulos (pastas), com REAPER.</h3>
+
+        <h2>Como funcionará nos serviços iSKGtm Prod?</h2>
+
+        <p>No serviço Prod, com Photoshop, onde é compatível com este fluxograma, utilizam-se
+        projetos com modulos (pastas de composição), que podem ser reutilizados com facilidade. </p>
+
+        <p>Ainda no serviço Prod, com DaVinci Resolve, onde é compatível com este fluxograma,
+        utilizam-se projetos com modulos (pastas de trabalho, timeline e composição), que
+        podem ser reutilizados com alguma facilidade, com uso reduzido de plugins de terceiros,
+        para melhor reutilização.</p>
+
+        <img src="/images/news/posts/outros/corecycle/1.jpg" alt="Exemplo de modulos, com DaVinci Resolve"/>
+
+        <h3>Exemplo de modulos, com DaVinci Resolve.</h3>
+
+        <h2>Como funcionará nos serviços iSKGtm Dev?</h2>
+
+        <p>No serviço Dev, utilizando ou não, Frameworks, linguagens como JavaScript, 
+        TypeScript ou Python, para determinadas situações, utilizam-se projetos com modulos
+        (ou componentes [React]), que podem ser utilizados com facilidade.</p>
+
+        <img src="/images/news/posts/outros/corecycle/2.jpg" alt="Exemplo de modulos, com VS Code"/>
+
+        <h3>Exemplo de modulos, com Visual Studio Code.</h3>
+
+        <h2>Observação a parceiros que utilizam nossos serviços iSKGtm Prod.</h2>
+
+        <p>Os serviços de produção, de vídeo, estão em transição, de um programa para outro, de VEGAS Pro para
+        DaVinci Resolve, iniciado em 14 jun. 2025. Vale destacar a incompatibilidade com esta
+        metodologia com VEGAS Pro, devido a problemas com projetos legados e ausência de elementos
+        modulares a serem criados. <br> Parceiros que mantem projetos legados deste programa (como
+        EagleVision e PlayVicio att: [06/08/2025]), serão mantidos até o final de 2026, mas não se enquadrará nos
+        novos padrões de fluxograma, com criação limitada e com serviços maior lentidão.</p>
 
         <h2>iSKGtm, We Are in Everything</h2>
       </main>
