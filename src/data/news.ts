782889130bf4391a992f4b688860ece9815f126e
Anthony Max <maximosenamaximo@gmail.com>
2025-08-21 18:25:13 -0300

    att: (1.0.0): add news post 2

--- src/data/news.ts
+++ src/data/news.ts
@@ -714,4 +714,51 @@ export const newsData: NewsItem[] = [
       </main>
     `,
   },
+
+    {
+    url: "/news/freed_from_desire_iskgtm_remix",
+    hide: true,
+    title: "Freed From Desire (iSKGtm Remix)",
+    label: "Freed From Desire, um novo remix bootleg, de Gala.",
+    imageUrl: "/images/news/posts/music/freed_from_desire_iskgtm_remix/banner.jpg",
+    tags: ["iSKGtm Music"],
+    tagImage: "/images/symb/music.png",
+    date: "09/01/2025",
+    authorName: "Anthony M.",
+    minutesRead: 3,
+    content: `
+      <main>
+        <img src="/images/news/tags/music.jpg" alt="iSKGtm Music"/>
+        
+        <p>Este remix foi lançado em Setembro de 2025, seguindo as regras de um bootleg. A voz destá música é de Gala, gravado originalmente em 1996 e lançada no mesmo ano.
+        </p>
+
+        <img src="/images/news/posts/music/freed_from_desire_iskgtm_remix/0.jpg"/>
+
+        <h3>Imagem do Álbum ∞ (iSKGtm). Imagem: Reprodução</h3>
+
+        <h2> Estrutura Sonora (parte técnica) </h2>
+
+        <p>
+          Este REMIX utiliza o 'drum kit' "Disco Tropical", especificadamente de "Beautiful People (iSKGtm Remix)" (Produção Nº52), que relembra um pouco como foi o sucesso de um estilo Funk ou Disco na década de 1970 (não confuda Funk Brasileiro com Disco), só que com os padrões e tecnologias modernas para a produção desta música, assim como, a nossa identidade sonora iSKGtm Music.
+
+          <br> Esta faixa REMIX, apesar da base ser a mesma da Produção Nº52, houve melhorias com padrões e masterização, bem como um novo '808' que utiliza baixos virtualizados de alta fidelidade.
+        </p>
+
+        <h2>Produção Nº53 e escolha da música</h2>
+
+        <p>
+          A música “Freed from Desire”, da cantora italiana Gala, foi lançada originalmente em 1996, e foi o sucesso nas pistas de dança em 1997 em diversos paises, com estilo de música moderna para época, chamada Techno Dance ou Euro Dance. <br>
+
+          Durante o Mundial de Clubes da FIFA de 2025, após 1 semana depois da finalização da produção Nº52 em Julho, a faixa "Freed From Desire" teve um pico de 850%, considerando apenas no Brasil, com a letra "nanananananaaaaaan nan". <br>
+          Ouvindo-a e comparando tecnicamente com a Produção Nº52, observei que a música ficaria excelente naquela base. No final de Junho, a produção foi iniciada, seguindo critérios rigorosos, tanto de parte legal, quanto de parte técnica, vale a pena conferir.
+        </p>
+
+        <iframe frameborder="0" scrolling="no" src="https://www.youtube.com/embed/1Lr89TR7YpY"></iframe>
+
+        <h2>iSKGtm, We Are in Everything</h2>
+      </main>
+    `,
+  },
+  
 ];
