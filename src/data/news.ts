af18d06713a4776c466645ac0d9efd0178be37f8
Anthony Max <maximosenamaximo@gmail.com>
2025-10-23 19:04:13 -0300

    att (1.2.0): new article post

--- src/data/news.ts
+++ src/data/news.ts
@@ -729,7 +729,7 @@ export const newsData: NewsItem[] = [
       <main>
         <img src="/images/news/tags/music.jpg" alt="iSKGtm Music"/>
         
-        <p>Este remix foi lançado em Setembro de 2025, seguindo as regras de um bootleg. A voz destá música é de Gala, gravado originalmente em 1996 e lançada no mesmo ano.
+        <p>Este remix foi lançado em Setembro de 2025, seguindo as regras de um bootleg. A voz desta música é de Gala, gravado originalmente em 1996 e lançada no mesmo ano.
         </p>
 
         <img src="/images/news/posts/music/freed_from_desire_iskgtm_remix/0.jpg"/>
@@ -758,6 +758,50 @@ export const newsData: NewsItem[] = [
         <h2>iSKGtm, We Are in Everything</h2>
       </main>
     `,
+  },
+
+  {
+  url: "/news/how_to_love_iskgtm_remix",
+  hide: true,
+  title: "How to Love (iSKGtm Remix)",
+  label: "How to Love, um novo remix bootleg, de Tokio Hotel.",
+  imageUrl: "/images/news/posts/music/how_to_love_iskgtm_remix/banner.jpg",
+  tags: ["iSKGtm Music"],
+  tagImage: "/images/symb/music.png",
+  date: "11/01/2025",
+  authorName: "Anthony M.",
+  minutesRead: 2,
+  content: `
+    <main>
+      <img src="/images/news/tags/music.jpg" alt="iSKGtm Music"/>
+      
+      <p>Este remix foi lançado em Novembro de 2025, seguindo as regras de um bootleg. A voz desta música é de Tokio Hotel, gravado originalmente e lançada em 2022.
+      </p>
+
+      <img src="/images/news/posts/music/how_to_love_iskgtm_remix/0.jpg"/>
+
+      <h3>Imagem do Álbum ∞ (iSKGtm). Imagem: Reprodução</h3>
+
+      <h2> Estrutura Sonora (parte técnica) </h2>
+
+      <p>
+        Este REMIX utiliza o classico 'drum kit' "Chill Tropical", que traz harmonias dançantes do Dance / House, mas com uma nova tecnologia de mixagem, com auxilio da utilização de IA, 
+        e claro, seguindo os padrões tecnicos e sonoros, assim como, a nossa identidade sonora iSKGtm Music.
+      </p>
+
+      <h2>Produção Nº54 e escolha da música</h2>
+
+      <p>
+        A música “How to Love”, de Tokio Hotel, é uma faixa lançada em 2022 dentro do álbum “2001”, que marca uma fase mais madura e introspectiva da banda. Nela, o grupo (liderado pelos irmãos Bill e Tom Kaulitz)
+        mescla synthpop, eletrônica e elementos alternativos, com uma produção moderna e bastante refinada.
+        <br> Com nosso Remix, a música que já era boa, 'deu' uma revitalizada, deixando ela com aspecto quente e dançante! Vale a pena conferir.
+      </p>
+
+      <iframe frameborder="0" scrolling="no" src="https://www.youtube.com/embed/-D6n4L2iEnw"></iframe>
+
+      <h2>iSKGtm, We Are in Everything</h2>
+    </main>
+  `,
   },
   
 ];
