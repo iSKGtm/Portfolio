1ea0cade3cf0341c4ae6256ebd8dd76e63b4b4a8
Anthony Max <maximosenamaximo@gmail.com>
2026-01-11 20:45:23 -0300

    feat (26.0.1): nova postagem

--- src/data/news.ts
+++ src/data/news.ts
@@ -27,6 +27,82 @@ export interface Article {
 
 export const newsData: NewsItem[] = [
 
+    {
+    url: "/artigo/10_anos",
+    hide: false,
+    title: "Especial de 10 Anos",
+    label: "Conteúdos para o Especial de 10 Anos iSKGtm",
+    imageUrl: "https://i.imgur.com/xHxXV9F.jpeg",
+    tagImage: "/images/symb/10y.png",
+    tags: ["iSKGtm 10"],
+    date: "01/12/2026",
+    authorName: "Anthony M.",
+    minutesRead: 3,
+    content: `
+    <main>
+      <img src="https://i.imgur.com/tsy6Kcb.jpeg" alt="iSKGtm 10 Anos"/>
+      
+      <p>
+      Este ano, para comemorar esses 10 anos de trabalhos, vamos relembrar a era clássica iSKGtm (ou iShakinGameR), postando conteúdos clássicos que marcaram uma história, na plataforma YouTube, onde tudo começou.
+      </p>
+
+      <h2>O que conteúdo seria?</h2>
+
+      <p>
+      A cada mês ano, cada época do que foi a iSKGtm será mostrada, indo nas eras 2016 até a era atual. <br>
+      Em 2016, o tipo de conteúdo consistia em gameplays de jogos como SA:MP, uma modificação para interação online de Grand Theft Auto: San Andreas, de Rockstar Games. <br> 
+      Esta era foi a mais nostalgica de todas, com o chamado voz do Google (de locução) e contendo diversos bordões como "Tomou na Jabiraca" que foi utilizado de forma massiva, que virou marca de iShakinGameR.
+      </p>
+
+      <img src="https://i.imgur.com/6S2wavr.jpeg"/>
+
+      <h3>Tomou na Jabiraca, bordão mais usado em 2016. Imagem: Acervo iSKGtm</h3>
+
+      <p>
+      Em 2017-18 expandimos para outros jogos, como Grand Theft Auto: 5 (Rockstar Games), Counter Strike: Global Offensive (Valve), The Forest (Endnight Games) e algumas vezes, com gameplays em grupo e comentados.
+      </p>
+
+      <p>
+      Em 2018, sendo fan do jogo "Sonic" e de suas músicas, passamos a produzir músicas em sons analógicos e sintetizados, que simulava o chip de áudio encontrado no console SEGA, o Mega Drive. <br>
+      No ano seguinte, com toda experiência técnica de criação desse tipo de música, produzimos nosso primeiro remix, mas não da forma que comum.
+      </p>
+
+      <img src="/images/news/sobre/music/1.jpg"/>
+
+      <h3>Álbum de música. Imagem: Acervo iSKGtm</h3>
+
+      <p>
+        - <q>De uma forma bem limitada, consegui fazer o primeiro remix, usando a música Levels, de Avicii. Eu curtia bastante esse tipo de remix em consoles antigos, e lancei o "Levels (Sonic Remix)", que usava os sintetizadores do Mega Drive (Yamaha YM2612 & Texas Instruments SN76489), como ocorreu com o famoso "Levels - Mario Remix", mas que usava um chip DSP do Super Nintendo (Sony SPC700).</q> <br>
+        <cite>Diz Anthony M. - responsável pela marca iSKGtm / iShaking Creative Media.</cite>
+      </p>
+      
+      <p>
+        Visto que a produção com chiptune foi aceito pelo público, analisamos o "terreno" das produções em bootleg remix daquela época e lançamos um bootleg em Abril Youngblood, de 5 Seconds of Summer. com a criação da divisão iSKGtm Music.
+      </p>
+
+      <p>
+        Ainda em 2019, até 2022 seguimos com músicas e gameplays. Mas em meados de 2022, criamos a divisão iSKGtm Dev, que é produções tecnológicas, como desenvolvimento de aplicação e web aplicação. Inicialmente, utilizamos JavaScript para nossas aplicações e em 2025 chegamos ao React TypeScript. Em 2026-27, analisaremos a possibilidade de desenvolver para áreas de back-end e posteriormente, para o Android, utilizando ReactNative e Flutter.
+      </p>
+
+      <img src="/images/carouselImages/slide8.jpg"/>
+
+      <h3>Webappp para Truco 24H. Imagem: Acervo iSKGtm</h3>
+
+      <p>
+        Tudo isso será mostrado no nosso Especial de 10 Anos, fiquem ligados!
+      </p>
+
+      <p>
+        Segue o link dos vídeos desse especial:
+      <br>
+        <a href="https://www.youtube.com/playlist?list=PL2DivU5yf-1wArFIBOAhkM8jBvT6YK5d_" target="_blank">Playlist do Especial 10 Anos</a>
+      </p>
+
+      <h2>iSKGtm, We Are in Everything</h2>
+    </main>
+  `,
+  },
+
   {
     url: "/artigo/ordinary-iskgtm-remix",
     hide: false,
