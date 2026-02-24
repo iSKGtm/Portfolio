cc30f43a080d7553b8f4350ee967ebb61803d8fa
Anthony Max <maximosenamaximo@gmail.com>
2026-02-23 21:14:04 -0300

    feat (26.1.2): Nova postagem.

--- src/data/news.ts
+++ src/data/news.ts
@@ -27,7 +27,57 @@ export interface Article {
 
 export const newsData: NewsItem[] = [
 
-    {
+  {
+    url: "/artigo/aint-nobody-iskgtm-remix",
+    hide: true,
+    title: "Ain't Nobody (iSKGtm Remix)",
+    label: "Ain't Nobody, um novo remix bootleg, de Alex Warren.",
+    imageUrl: "https://i.imgur.com/qYc0xAx.jpeg",
+    tags: ["iSKGtm Music"],
+    tagImage: "/images/symb/music.png",
+    date: "03/01/2026",
+    authorName: "Anthony M.",
+    minutesRead: 5,
+    content: `
+    <main>
+      <img src="/images/news/tags/music.jpg" alt="iSKGtm Music"/>
+      
+      <p>
+      Este remix é o segundo do album ∞ (2026), seguindo as regras de um bootleg. Voltamos ao túnel do tempo, a voz desta música é de Chaka Khan, lançada originalmente em 1983.
+      </p>
+
+      <img src="https://i.imgur.com/qYc0xAx.jpeg"/>
+
+      <h3>Imagem do Álbum ∞ 2026 (iSKGtm). Imagem: Reprodução</h3>
+
+      <h2> Estrutura sonora (parte técnica) </h2>
+
+      <p>
+        Este REMIX utiliza a versão mais recente do drum kit "Disco Tropical", que relembra um pouco como foi o sucesso de um estilo Funk ou Disco na década de 1970 (não confuda Funk Brasileiro com Disco), só que com os padrões e tecnologias modernas para a produção desta música, assim como, a nossa identidade sonora iSKGtm Music. Vale ressaltar que esta é a quarta versão deste drum kit, que vem sendo aprimorado, com suporte há 96KHz/24bit Hi-Res. <br>
+
+        Na teoria, seria a sexta versão, se incluir faixas como: "Sweet & Bitter (iSKGtm Remix)" & Disco Tropical (2022), mas essas faixas usavam técnicas obsoletas e mais simples de mixagem & masterização, como também, usavam mais samples do "Chill Tropical" do que de fato os de Disco, sendo necessário refazer todo o drum kit "Disco Tropical", ocorrendo com lançamento de "Beautiful People (iSKGtm Remix)".
+      </p>
+
+      <h2>Produção Nº56 e escolha da música</h2>
+
+      <p>
+        Lançada em 1983, "Ain't Nobody" foi incluída no álbum ao vivo "Stopin' at the Savoy - Live". Apesar de o disco ser majoritariamente gravado em apresentações, a canção foi uma das quatro faixas inéditas de estúdio adicionadas ao projeto. Curiosamente, ela marcou um dos últimos grandes momentos de Chaka Khan com a banda Rufus antes de ela consolidar sua carreira solo de vez. <br>
+
+        A música não foi apenas um "hit", e sim, um hino cultural. Alcançando o 1º lugar na Billboard R&B Chart, e no topo das paradas em diversos países.
+
+        <br> A escolha desta faixa se deve ao fato de que ela é perfeita para remixes, pois é uma faixa versatil, onde se encaixa perfeitamente em sons modernos em dance ou disco, sendo assim, optamos pelo uso do drum kit "Disco Tropical" (feita em cima dos ativos de "Ordinary (iSKGtm Remix)"), mas com a velocidade reduzida a 110.00 BPM. <br>
+        
+        Exploramos também o vocal chop, como do Remix anterior, com técnicas de sons "experimentais", mas com ar "comercial", como também, tecnicas de "Strum" usando um baixo virtualizado, mas com sonorização geral sintetica, dando um ar de "groove hibrido", que é "reaproveitavel" em outros estilos sonoros, vale a pena conferir.
+      </p>
+
+      <iframe frameborder="0" scrolling="no" src="https://www.youtube.com/embed/1YUo1nJp7HI"></iframe>
+
+      <h2>iSKGtm, We Are in Everything</h2>
+    </main>
+  `,
+  },
+
+  {
     url: "/artigo/10_anos",
     hide: false,
     title: "Especial de 10 Anos",
